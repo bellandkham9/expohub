b5b2c214a0e6182a561d6c745f9a1a73701d52cc
KarlTrafi <karltrafi@gmail.com>
2025-08-06 20:00:30 +0100

    ajout les modifications combiner

--- plateforme/app/Http/Middleware/Authenticate.php
+++ plateforme/app/Http/Middleware/Authenticate.php
@@ -6,7 +6,6 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-<<<<<<< HEAD
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
@@ -16,12 +15,5 @@ class Authenticate extends Middleware
             return route('auth.connexion');
         }
         return null;
-=======
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('auth.connexion'); // ta route de connexion personnalisée
-        }
->>>>>>> 08732e26cc269c5ffb80aae87775bcfd99b1805c
     }
 }
